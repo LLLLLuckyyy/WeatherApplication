d3b676aa0578d729cbd02e467573adddf629f497
Mikhail_Chelpanov <mikhailchelpanov20@gmail.com>
2021-07-11 10:07:28 +0300

    Updated

--- WeatherApplication.Domain.Core/StatisticalModel.cs
+++ WeatherApplication.Domain.Core/StatisticalModel.cs
@@ -10,28 +10,37 @@ namespace WeatherApplication.Domain.Core
         public int Id { get; set; }
 
         [Required]
+        [Range(-50, 50)]
         public double MaxTemperature { get; set; }
         [Required]
+        [Range(-50, 50)]
         public double MinTemperature { get; set; }
         [Required]
+        [Range(-50, 50)]
         public double AverageTemperature { get; set; }
         [Required]
         public string CurrentTemperature { get; set; }
 
         [Required]
+        [Range(0, 100)]
         public double MaxRainProbability { get; set; }
         [Required]
+        [Range(0, 100)]
         public double MinRainProbability { get; set; }
         [Required]
+        [Range(0, 100)]
         public double AverageRainProbability { get; set; }
         [Required]
         public string CurrentRainProbability { get; set; }
 
         [Required]
+        [Range(0, 40)]
         public double MaxWindForce { get; set; }
         [Required]
+        [Range(0, 40)]
         public double MinWindForce { get; set; }
         [Required]
+        [Range(0, 40)]
         public double AverageWindForce { get; set; }
         [Required]
         public string CurrentWindForce { get; set; }
--- WeatherApplication.Domain.Core/WeatherModel.cs
+++ WeatherApplication.Domain.Core/WeatherModel.cs
@@ -8,11 +8,17 @@ namespace WeatherApplication.Domain.Core
     {
         [Key]
         public int Id { get; set; }
+        //In Celsius degrees
         [Required]
+        [Range(-50, 50)]
         public double Temperature { get; set; }
+        //In percents
         [Required]
+        [Range(0, 100)]
         public double RainProbability { get; set; }
+        //meters per second
         [Required]
+        [Range(0, 40)]
         public double WindForce { get; set; }
         [Required]
         public DateTime ObservationTime { get; set; }
